c934b4e99149f9818488771c3c8954950e9f0d2b
Laurent Baysse (Lolo_32) <lol.b@free.fr>
2012-04-24 01:20:46 +0200

    Fini avec les pages statiques

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :development do
 end
 
 group :test do
-  gem 'rspec', '2.5.0'
-  gem 'webrat', '0.7.1'
+  gem 'rspec'	#, '2.5.0'
+  gem 'webrat'	#, '0.7.1'
+
+  gem 'spork'	#, '0.9.0.rc5'
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
+# encoding: utf-8
 class PagesController < ApplicationController
   def home
+    @titre = "Accueil"
   end
 
   def contact
+    @titre = "Contact"
+  end
+
+  def about
+    @titre = "À propos"
   end
 end
