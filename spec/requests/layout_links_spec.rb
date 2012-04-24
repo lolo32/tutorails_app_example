a5ddb97dcebe11a8c80ebd180b3569f927050c85
Laurent Baysse (Lolo_32) <lol.b@free.fr>
2012-04-24 18:40:56 +0200

    Correction d'un boggue dans le test pour la page "/about"

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -8,17 +8,17 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Accueil")
   end
 
-  it "devrait trouver une page Contact at '/contact'" do
+  it "devrait trouver une page Contact à '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
 
-  it "should have an À Propos page at '/about'" do
+  it "devrait trouver une page À Propos à '/about'" do
     get '/about'
-    response.should have_selector('title', :content => "À Propos")
+    response.should have_selector('title', :content => "À propos")
   end
 
-  it "devrait trouver une page Iade à '/help'" do
+  it "devrait trouver une page Aide à '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Aide")
   end
