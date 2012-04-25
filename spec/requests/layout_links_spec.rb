60ad7a9408e70654bcc3bcd4a30e4045675e33a2
Laurent Baysse (Lolo_32) <lol.b@free.fr>
2012-04-25 02:03:58 +0200

    Tests des liens des pages ajoutés

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,4 +27,18 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Inscription")
   end
+
+  it "devrait avoir le bon lien sur le layout" do
+    visit root_path
+    click_link "À Propos"
+    response.should have_selector('title', :content => "À propos")
+    click_link "Aide"
+    response.should have_selector('title', :content => 'Aide')
+    click_link "Contact"
+    response.should have_selector('title', :content => 'Contact')
+    click_link "Accueil"
+    response.should have_selector('title', :content => 'Accueil')
+    click_link "S'inscrire !"
+    response.should have_selector('title', :content => "Inscription")
+  end
 end
